5981f5c60ad2093a6c4beb40f6859922391285e6
DStark <hindash@email.arizona.edu>
2020-02-29 14:40:28 -0700

    jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
+var localConnection = {
   host: "localhost",
   port: 3306,
   user: process.env.user,
   password: process.env.db,
   database: "burgers_db"
-});
+}
+var connection = mysql.createConnection(process.env.JAWSDB_URL || localConnection);
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
